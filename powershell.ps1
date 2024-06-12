bffa9491af96dc3151f10edcd4ee12ddb7bd2718
github-cloudlabsuser-555 <167313238+github-cloudlabsuser-555@users.noreply.github.com>
2024-06-12 05:46:13 +0000

    Copilot third commit

--- powershell.ps1
+++ powershell.ps1
@@ -10,9 +10,4 @@ $storageAccountName = "mystorageaccount"
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 # Create a storage account
-New-AzStorageAccount -ResourceGroupName $resourceGroupName `
-  -Name $storageAccountName `
-  -Location $location `
-  -SkuName Standard_LRS `
-  -Kind StorageV2 `
-  -EnableHttpsTrafficOnly 1
+New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_LRS
